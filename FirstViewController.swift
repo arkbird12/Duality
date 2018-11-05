5df21ff929759e5cec122fd54816f58873ff33a0
arkbird12 <rishi.r.thakkar@gmail.com>
2018-11-05 09:30:15 -0800

    Initial commit

---
+++ FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Duality
+//
+//  Created by rishi on 10/30/18.
+//  Copyright © 2018 Rishi Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
